9e2d500fd1fc646e9c77c7429c490ed25408cc48
Nguyen Son Linh <linhns59@gmail.com>
2025-03-25 03:15:36 +0800

    feat: use game loop class

--- pybattletank/__main__.py
+++ pybattletank/__main__.py
@@ -1,60 +1,12 @@
-import random
+import pygame
+
+from pybattletank.game import Game
 
 
-def init():
-    return "init", random.randint(1, 10)
-
-
-def processInput():
-    while True:
-        word = input("What is the magic number? ")
-        if word == "quit":
-            return None
-        try:
-            number = int(word)
-            break
-        except ValueError:
-            print("Please type a number without decimals!")
-            continue
-    return number
-
-
-def update(status, magic_number, player_number):
-    if player_number is None:
-        status = "end"
-    elif player_number == magic_number:
-        status = "win"
-    elif magic_number < player_number:
-        status = "lower"
-    elif magic_number > player_number:
-        status = "higher"
-    return status, magic_number
-
-
-def render(status, magic_number):
-    if status == "win":
-        print("This is correct! You win!")
-    elif status == "end":
-        print("Bye!")
-    elif status == "lower":
-        print("The magic number is lower")
-    elif status == "higher":
-        print("The magic number is higher")
-    else:
-        msg = f"Unexpected status: {status}"
-        raise RuntimeError(msg)
-
-
-def run():
-    status, magic_number = init()
-    while status not in ["win", "end"]:
-        guess = processInput()
-        status, magic_number = update(status, magic_number, guess)
-        render(status, magic_number)
-
-
-def main():
-    run()
+def main() -> None:
+    game = Game()
+    game.run()
+    pygame.quit()
 
 
 if __name__ == "__main__":
